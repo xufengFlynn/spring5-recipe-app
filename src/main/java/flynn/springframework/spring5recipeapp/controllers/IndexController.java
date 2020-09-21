35848c4b78fd5a17a7a8484ca5bc5fb259207561
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-21 16:57:11 -0700

    adding database initialization script

--- src/main/java/flynn/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/flynn/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,12 +1,34 @@
 package flynn.springframework.spring5recipeapp.controllers;
 
+import flynn.springframework.spring5recipeapp.domain.Category;
+import flynn.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import flynn.springframework.spring5recipeapp.repositories.CategoryRepository;
+import flynn.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/flynn/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/flynn/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package flynn.springframework.spring5recipeapp.repositories;
 import flynn.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
