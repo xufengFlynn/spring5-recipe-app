09ffe810a367a90d1ff73589378e47b5f4a5e532
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-24 09:15:01 -0700

    Returning result of recipeRepository.findAll() when calling getRecipes()

---
+++ src/test/java/flynn/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package flynn.springframework.spring5recipeapp.services;
+
+import flynn.springframework.spring5recipeapp.domain.Recipe;
+import flynn.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
