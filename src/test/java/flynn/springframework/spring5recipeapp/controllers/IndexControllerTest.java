7b6ca2783a030d6a27d4350d418b325a65212241
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-24 09:33:21 -0700

    Test for IndexController

---
+++ src/test/java/flynn/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,39 @@
+package flynn.springframework.spring5recipeapp.controllers;
+
+import flynn.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+
+        String viewName = controller.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
