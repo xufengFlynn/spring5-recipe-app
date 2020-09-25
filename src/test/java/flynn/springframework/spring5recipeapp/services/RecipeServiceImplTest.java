f9960ca76bc011b4ebb772c6286871f7841a28d6
xufengFlynn <zhangxufeng26@gmail.com>
2020-09-25 15:35:31 -0700

    Displayed the recipe by ID

--- src/test/java/flynn/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/flynn/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -8,8 +8,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -39,6 +41,23 @@ class RecipeServiceImplTest {
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
+
+    }
+
+    @Test
+    void findById() {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
 
     }
 }
