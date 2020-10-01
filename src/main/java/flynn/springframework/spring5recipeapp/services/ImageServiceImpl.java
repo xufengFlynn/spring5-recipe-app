e8df8588a6c1324be25cf4ac7ad7a1474cba0f4f
xufengFlynn <zhangxufeng26@gmail.com>
2020-10-01 14:18:08 -0700

    Display images from database

---
+++ src/main/java/flynn/springframework/spring5recipeapp/services/ImageServiceImpl.java
@@ -0,0 +1,41 @@
+package flynn.springframework.spring5recipeapp.services;
+
+import flynn.springframework.spring5recipeapp.domain.Recipe;
+import flynn.springframework.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class ImageServiceImpl implements ImageService {
+
+    RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error Occurred", e);
+            e.printStackTrace();
+        }
+    }
+}
