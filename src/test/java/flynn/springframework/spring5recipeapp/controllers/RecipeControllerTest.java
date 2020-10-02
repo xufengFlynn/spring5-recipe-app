85cfc4da104c572574d5105e91c456929f948701
xufengFlynn <zhangxufeng26@gmail.com>
2020-10-01 17:20:42 -0700

    Added Number Format Exception Handler.

--- src/test/java/flynn/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/flynn/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -60,7 +60,16 @@ class RecipeControllerTest {
 
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-        mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
+    }
+
+    @Test
+    void testGetRecipeNumberFormatException() throws Exception {
+//        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asd/show")).andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
