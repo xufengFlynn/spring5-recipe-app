408b5fd02c65b27a8f75a6e72b4b94724cd03326
xufengFlynn <zhangxufeng26@gmail.com>
2020-10-01 17:39:45 -0700

    Added @ControllerAdvise to handle global number format exceptions.

--- src/main/java/flynn/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/flynn/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -58,29 +58,12 @@ public class RecipeController {
     @ExceptionHandler(NotFoundException.class)
     public ModelAndView handleNotFound(Exception exception){
 
-        log.error("Handling not found exception");
+        log.error("Handling Not Found Exception");
         log.error(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("404error");
-
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-
-        log.error("Handling Number Format Exception");
-        log.error(exception.getMessage());
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
